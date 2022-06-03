c0dd5c9e7449b3f8c8aa00eae79f8f7b2950832c
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-06-03 12:17:59 -0500

    Creación de objeto para la instancia de retrofit

---
+++ dataAccess/src/main/java/com/ceiba/dataaccess/network/ApiInstance.kt
@@ -0,0 +1,23 @@
+package com.ceiba.dataaccess.network
+
+import com.ceiba.dataaccess.api.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiInstance {
+    const val URL = "https://api.mercadolibre.com/sites/MLA"
+    fun createApi(): ApiService {
+        val okHttpBuilder = OkHttpClient.Builder()
+        okHttpBuilder.addInterceptor(HttpLoggingInterceptor())
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpBuilder.build())
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
+}
