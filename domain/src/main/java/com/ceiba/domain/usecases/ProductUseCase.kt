d0c5f587cd180528bb1b6f61f28e8df008cb9f1c
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-06-03 08:00:42 -0500

    Agregado de método que implementa la obtención de los productos en los casos de uso

--- domain/src/main/java/com/ceiba/domain/usecases/ProductUseCase.kt
+++ domain/src/main/java/com/ceiba/domain/usecases/ProductUseCase.kt
@@ -1,4 +1,8 @@
 package com.ceiba.domain.usecases
 
-class ProductUseCase {
+import com.ceiba.domain.models.Product
+import com.ceiba.domain.repository.ProductRepository
+
+class ProductUseCase constructor(private val productRepository: ProductRepository) {
+    suspend fun getProducts(): List<Product> = productRepository.getProducts()
 }
