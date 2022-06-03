70532f5c64b8d488c3d8488854776664e4dba9b8
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-06-03 09:06:38 -0500

    Creación de validaciones en caso de que llegue la información del producto esté vacía y se agregan pruebas unitarias de este caso

--- domain/src/main/java/com/ceiba/domain/models/Product.kt
+++ domain/src/main/java/com/ceiba/domain/models/Product.kt
@@ -1,6 +1,22 @@
 package com.ceiba.domain.models
 
+import com.ceiba.domain.exception.ProductException
+
 open class Product(
     open var title: String,
     open var price: Int,
-    open var imageProduct: String)
+    open var imageProduct: String) {
+    companion object {
+        const val MESSAGE_EMPTY = "Tenemos algunos inconvenientes, por favor intente de nuevo"
+    }
+
+    init {
+        validateProductEmpty()
+    }
+
+    private fun validateProductEmpty() {
+        if (title.isEmpty() || imageProduct.isEmpty()) {
+            throw ProductException(MESSAGE_EMPTY)
+        }
+    }
+}
--- domain/src/test/java/com/ceiba/domain/ProductTest.kt
+++ domain/src/test/java/com/ceiba/domain/ProductTest.kt
@@ -1,4 +1,27 @@
 package com.ceiba.domain
 
+import com.ceiba.domain.core.ProductBuilder
+import com.ceiba.domain.exception.ProductException
+import com.ceiba.domain.models.Product
+import org.junit.Assert
+import org.junit.Test
+
 class ProductTest {
+    @Test
+    fun product_validateDataEmpty_isFailure() {
+        //Arrange
+        val productBuilder = ProductBuilder.aProduct()
+            .withTitle("")
+            .withImageProduct("")
+
+        val expectedMessage = Product.MESSAGE_EMPTY
+
+        try {
+            //Act
+            productBuilder.build()
+        } catch (exception: ProductException) {
+            //Assert
+            Assert.assertEquals(expectedMessage, exception.message)
+        }
+    }
 }
