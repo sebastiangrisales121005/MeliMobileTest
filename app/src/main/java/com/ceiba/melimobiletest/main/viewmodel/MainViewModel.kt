e3ca8381d22f826dd41ab46efc76cff8d8449236
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-06-06 09:27:03 -0500

    Se agrega implemetación de la inyección de dependencias al viewmodel

--- app/src/main/java/com/ceiba/melimobiletest/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ceiba/melimobiletest/main/viewmodel/MainViewModel.kt
@@ -1,4 +1,12 @@
 package com.ceiba.melimobiletest.main.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import com.ceiba.domain.usecases.ProductUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private val productUseCase: ProductUseCase): ViewModel() {
+
+    
 }
