f8e43e6ae18ede58e8cb5c95915f3cd88d8f89ef
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-06-06 09:35:19 -0500

    Declaración de variables necesarias para el uso en el MainActivity

--- app/src/main/java/com/ceiba/melimobiletest/main/MainActivity.kt
+++ app/src/main/java/com/ceiba/melimobiletest/main/MainActivity.kt
@@ -3,24 +3,29 @@ package com.ceiba.melimobiletest.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import com.ceiba.domain.usecases.ProductUseCase
 import com.ceiba.melimobiletest.R
+import com.ceiba.melimobiletest.databinding.ActivityMainBinding
+import com.ceiba.melimobiletest.main.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject
-    lateinit var productUseCase: ProductUseCase
+
+    lateinit var mActivityMainBinding: ActivityMainBinding
+    private val mMainViewModel: MainViewModel? by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mActivityMainBinding.root)
 
-      CoroutineScope(Dispatchers.Main).launch {
-          withContext(Dispatchers.IO) {
-               productUseCase.getProducts()
-          }
-      }
+    }
+
+    fun initializeWidgets() {
+
     }
 }
--- app/src/main/java/com/ceiba/melimobiletest/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ceiba/melimobiletest/main/viewmodel/MainViewModel.kt
@@ -8,5 +8,5 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(private val productUseCase: ProductUseCase): ViewModel() {
 
-    
+
 }
