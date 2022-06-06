45e4065153a24674388267a30963bf708f150fa4
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-06-06 09:54:21 -0500

    Configuración del uso del adapter en la vista principal con sus prespectivas propiedades

--- app/src/main/java/com/ceiba/melimobiletest/main/MainActivity.kt
+++ app/src/main/java/com/ceiba/melimobiletest/main/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ceiba.domain.models.Product
 import com.ceiba.domain.usecases.ProductUseCase
 import com.ceiba.melimobiletest.R
+import com.ceiba.melimobiletest.adapter.MainAdapter
 import com.ceiba.melimobiletest.databinding.ActivityMainBinding
 import com.ceiba.melimobiletest.main.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,14 +21,29 @@ class MainActivity : AppCompatActivity() {
     lateinit var mActivityMainBinding: ActivityMainBinding
     private val mMainViewModel: MainViewModel? by viewModels()
 
+    lateinit var mMainAdapter: MainAdapter
+    private val listArrayOfProducts = ArrayList<Product>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mActivityMainBinding.root)
 
+        initializeWidgets()
+
     }
 
-    fun initializeWidgets() {
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
 
+    private fun initializeWidgets() {
+        mMainAdapter = MainAdapter(this, listArrayOfProducts)
+
+        with(mActivityMainBinding.listProducts) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = mMainAdapter
+        }
     }
 }
