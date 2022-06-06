dc275a5921e887be1520af530a930e39f59b4b93
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-06-06 07:31:31 -0500

    Agregado de prueba unitaria al obtener la lista de productos simulada y se agregan las respectivas librerias para su uso

--- domain/src/test/java/com/ceiba/domain/ProductTest.kt
+++ domain/src/test/java/com/ceiba/domain/ProductTest.kt
@@ -4,8 +4,13 @@ import com.ceiba.domain.core.ProductBuilderTest
 import com.ceiba.domain.exception.ProductException
 import com.ceiba.domain.models.Address
 import com.ceiba.domain.models.Product
+import com.ceiba.domain.repository.ProductRepository
+import com.ceiba.domain.usecases.ProductUseCase
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 
 class ProductTest {
     @Test
@@ -38,5 +43,23 @@ class ProductTest {
             //Assert
             Assert.assertEquals(expectedMessage, exception.message)
         }
+    }
+
+    @Test
+    fun product_getAllProducts_isSuccess() = runTest {
+        //Arrange
+        val productBuilder = ProductBuilderTest.aProduct()
+            .build()
+        val productList = listOf(productBuilder)
+        val productRepository = Mockito.mock(ProductRepository::class.java)
+        `when`(productRepository.getProducts()).thenReturn(productList)
+        val productUseCase = ProductUseCase(productRepository)
+
+        //Act
+        val productsFromUseCase = productUseCase.getProducts()
+
+        //Assert
+        Assert.assertEquals(1, productsFromUseCase.size)
+
     }
 }
