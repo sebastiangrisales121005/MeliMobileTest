98294f098be0de6b54c5d3c902399835f0a7b4b0
Juan Sebastian Grisales Muñoz <sebastian.grisales@ceiba.com.co>
2022-06-06 15:11:10 -0500

    Agregado de viewBinding e inicialización del adapater para mostrar las especificaciones en el detalle de producto

--- app/src/main/java/com/ceiba/melimobiletest/detailproduct/view/DetailProductActivity.kt
+++ app/src/main/java/com/ceiba/melimobiletest/detailproduct/view/DetailProductActivity.kt
@@ -1,12 +1,32 @@
 package com.ceiba.melimobiletest.detailproduct.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ceiba.melimobiletest.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ceiba.domain.models.Specs
+import com.ceiba.melimobiletest.adapter.DetailProductAdapter
+import com.ceiba.melimobiletest.databinding.ActivityDetailProductBinding
 
 class DetailProductActivity : AppCompatActivity() {
+    lateinit var mActivityDetailProductBinding: ActivityDetailProductBinding
+    lateinit var mDetailProductAdapter: DetailProductAdapter
+
+    private val listArraySpecs = ArrayList<Specs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_product)
+        mActivityDetailProductBinding = ActivityDetailProductBinding.inflate(layoutInflater)
+        setContentView(mActivityDetailProductBinding.root)
+
+        initializeWidgets()
+    }
+
+    private fun initializeWidgets() {
+        mDetailProductAdapter = DetailProductAdapter(this, listArraySpecs)
+        with(mActivityDetailProductBinding.listSpecs) {
+            layoutManager = LinearLayoutManager(this@DetailProductActivity)
+            adapter = mDetailProductAdapter
+        }
+
     }
 }
